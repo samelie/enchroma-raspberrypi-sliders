2e5641a07bec06dd9d0f09bab7732e30f575feac
samelie <sam@stinkdigital.com>
2018-02-15 20:48:41 -0800

    python fix

--- pyth.py
+++ pyth.py
@@ -1,11 +1,21 @@
 from time import sleep
 import sys, json
+import Adafruit_GPIO.SPI as SPI
+import Adafruit_MCP3008
 
 def main():
-    i = 0
+    CLK  = 18
+    MISO = 23
+    MOSI = 24
+    CS   = 25
+    SPI_PORT   = 0
+    SPI_DEVICE = 0
+    mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
     while True:
-        print(i)
-        i += 1
+        adcR = mcp.read_adc(0)
+        adcG = mcp.read_adc(1)
+        adcB = mcp.read_adc(2)
+        print str(adcR) + " " + str(adcG) + " " + str(adcB)
         sys.stdout.flush()
         sleep(0.02)
 
